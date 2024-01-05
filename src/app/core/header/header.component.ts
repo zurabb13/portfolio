f5a1797ccda1f291d9359f71530b74ad74d758a6
zura <zura.baramia98@outlook.com>
2024-01-05 19:08:17 +0400

    create header

---
+++ src/app/core/header/header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { NavigationComponent } from './header/navigation/navigation.component';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [NavigationComponent],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss',
+})
+export class HeaderComponent {}
