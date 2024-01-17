20e4c916fbb2cef34fcc4c9e681dd2d6e71699a3
zura <zura.baramia98@outlook.com>
2024-01-17 12:49:18 +0400

    about component

--- src/app/core/body/body/body.component.ts
+++ src/app/core/body/body/body.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { BannerComponent } from './banner/banner.component';
+import { AboutComponent } from './about/about.component';
 
 @Component({
   selector: 'app-body',
   standalone: true,
-  imports: [BannerComponent],
+  imports: [BannerComponent, AboutComponent],
   templateUrl: './body.component.html',
   styleUrl: './body.component.scss',
 })
